84aa7deb5fc7f78944b2430ade99ec8ed1f5d849
Kyle Marchuk <kdmarchuk@gmail.com>
2019-08-19 16:55:00 -0700

    Images
    Fold for GUI images

---
+++ micro_manager_controls.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 19 16:34:41 2019
+
+File to utilize micro manager in setting up ROI generator
+
+@author: Kyle
+"""
+
+def snapTaggedImage(self):
+    
